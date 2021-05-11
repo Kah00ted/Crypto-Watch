b25772346f68b535ae5586622c469bafd0323582
Reid Metzger <contact@reidmetzger.com>
2021-05-11 12:59:38 -0400

    buy sell hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 # ---user config---
 #coin
@@ -7,6 +8,10 @@ symbol = "safemoon"
 
 #lunarcrush api
 apiKey = "xwe7noxgswhw5cbqhjnnrm"
+
+#pricing to notify
+notifLow = 0.000004
+notifHigh = 0.000008
 
 url = "https://api.lunarcrush.com/v2/?key="+apiKey+"&symbol="+symbol+"&data=assets"
 
@@ -16,5 +21,18 @@ headers = {}
 response = requests.request("GET", url, headers=headers, data=payload)
 
 coinJson = json.loads(response.text)
+coinPrice = coinJson["data"][0]["price"]
 
 print('{0:.10f}'.format(coinJson["data"][0]["price"]))
+
+def checkCrypto():
+    if (coinPrice <= notifLow):
+        print("buy")
+    elif (coinPrice >= notifHigh):
+        print("sell")
+    else:
+        print("hold")
+
+while (5>1):
+    checkCrypto()
+    time.sleep(3)
